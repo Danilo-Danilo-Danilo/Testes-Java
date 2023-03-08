83868c1a5e450f298db0504b193328143e0ca263
Danilo <danilo2lacerda@gmail.com>
2023-03-08 14:58:35 -0300

    Changes to be committed: 	new file:   .vscode/settings.json 	new file:   README.md 	modified:   bin/App.class 	modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import javax.print.event.PrintEvent;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello world");
+        System.out.println(
+            "Integer: " + 10 +
+            " Double: " + 3.14 +
+            " Boolean: " + true);
+            Scanner scanner = new Scanner(System.in);
+            String estrigue = String.format("%s may prefer %s.", "Or you", "String.format()");
+            System.out.println(estrigue);
+            int i=0;
+            i++;
+            System.out.println(i++);
+            System.out.println(i);
     }
-}
+    
+    }
